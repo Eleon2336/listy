1afb726bfe9eb51945a16c87139be3819a5eb587
Eleon2336 <1992eleon@gmail.com>
2024-01-01 14:52:18 -0800

    moved colors from app to index - it removed errors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,13 @@
 import './App.css';
-import { createTheme, ThemeProvider } from '@mui/material';
-import { amber, cyan, deepOrange, green, pink } from '@mui/material/colors';
 import Header from '../Header/Header';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: cyan[700] //for primary interface elements
-    },
-    secondary: {
-      main: pink[400]
-    },
-    error: {
-      main: amber
-    },
-    warning: {
-      main: deepOrange
-    },
-    info: {
-      main: green
-    },
-    success: {
-      main: deepOrange[300]
-    }
-  }
-})
+
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
       <div className="App">
         <Header />
       </div>
-    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { amber, cyan, deepOrange, green, pink } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: cyan[700] //for primary interface elements
+    },
+    secondary: {
+      main: pink[400]
+    },
+    error: amber,
+    warning: deepOrange,
+    info: green,
+    success: {
+      main: deepOrange[300]
+    }
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-
