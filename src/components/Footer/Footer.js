146ef571e7fe17772b8317f30b0cd843a08e1dcb
Eleon2336 <1992eleon@gmail.com>
2024-01-01 22:28:03 -0800

    box added to the three components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,9 @@
+import Box from '@mui/system/Box';
+
 export default function Footer(props) {
     return (
-      <>
+      <Box>
         <h1>Footer Component</h1>
-      </>
+      </Box>
     )
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,9 @@
+import Box from '@mui/system/Box';
+
 export default function Header(props) {
   return (
-    <>
+    <Box>
       <h1>Header Component</h1>
-    </>
+    </Box>
   )
 }
