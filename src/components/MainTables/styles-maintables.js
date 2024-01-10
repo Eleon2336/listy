af15195a0403dac434238efb2b4f910701ab884c
Eleon2336 <1992eleon@gmail.com>
2024-01-09 16:41:41 -0800

    divs style changes

--- src/components/MainTables/styles-maintables.js
+++ src/components/MainTables/styles-maintables.js
@@ -3,25 +3,29 @@ import { Colors } from "../../theme/theme";
 import styled from "@emotion/styled";
 
 //container
-export const MainDiv = styled(Container)(() => ({
+export const MainDiv = styled(Container)(({ theme }) => ({
     flex: "6",
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
-    gap: "1rem",
     padding: "0 0 1.5rem !important",
     margin: "0 !important",
-    maxWidth: "none !important"
+    maxWidth: "none !important",
+    [theme.breakpoints.down("sm")]: {
+        gap: "1rem"
+    }
 }));
 
 export const ListDiv = styled(Container)(() => ({
     flex: "1",
-    padding: "0"
+    padding: "0",
+    marginLeft: "1.2rem"
 }));
 
 export const DetailsDiv = styled(Container)(() => ({
     flex: "1",
-    padding: "0"
+    padding: "0",
+    marginRight: "1.2rem"
 }));
 
 export const ListPaper = styled(Paper)(() => ({
