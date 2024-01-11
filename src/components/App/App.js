30a2e4f93567f37f0355865357b887531b583867
Eleon2336 <1992eleon@gmail.com>
2024-01-11 10:59:32 -0800

    updated location text to match pathname

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import { useTheme } from "@emotion/react";
+import { useLocation } from "react-router-dom";
 import SearchAppBar from "../SearchAppBar/SearchAppBar";
 import MainTables from "../MainTables/MainTables";
 import Footer from "../Footer/Footer";
@@ -10,6 +11,29 @@ import {
 
 function App() {
   const theme = useTheme();
+  const location = useLocation();
+  const { pathname } = location;
+
+  let locText = '';
+  switch (pathname) {
+    case '/':
+      locText = 'Select a Pokemon type';
+      break;
+    case '/normal':
+      locText = 'Normal Types';
+      break;
+    case '/grass':
+      locText = 'Grass Types';
+      break;
+    case '/fire':
+      locText = 'Fire Types';
+      break;
+    case '/water':
+      locText = 'Water Types';
+      break;
+    default:
+      locText = 'Page not found';
+  }
 
   return (
     <>
@@ -20,7 +44,7 @@ function App() {
             variant="h5"
             color={theme.palette.primary.main}
           >
-            Normal Types
+            {locText}
           </HeaderText>
         </HeaderContainer>
         <MainTables />
