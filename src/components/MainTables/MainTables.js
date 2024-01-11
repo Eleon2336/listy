c104ab628c41c7adf76d00189f07efd560bab806
Eleon2336 <1992eleon@gmail.com>
2024-01-10 19:22:16 -0800

    rendered pokemon list through map method

--- src/components/MainTables/MainTables.js
+++ src/components/MainTables/MainTables.js
@@ -1,5 +1,9 @@
-import { DetailsDiv, DetailsList, DetailsPaper, ListDiv, ListPaper, MainDiv, PokemonList, TheListItem } from "./styles-maintables";
+import { DetailsDiv, ListDiv, ListPaper, MainDiv, PokemonList, TheListItem, ListItemText } from "./styles-maintables";
 import DataTable from "../DataTable/DataTable";
+
+const pokeList = [
+    "Rattata", "Eevee", "Meowth", "Ditto", "Taillow", "Patrat", "Deerling", "Stufful", "Wooloo", "Lechonk"
+];
 
 export default function MainTables() {
     return (
@@ -7,39 +11,17 @@ export default function MainTables() {
             <ListDiv>
                 <ListPaper>
                     <PokemonList>
-                        <TheListItem>Rattata</TheListItem>
-                        <TheListItem>Eevee</TheListItem>
-                        <TheListItem>Meowth</TheListItem>
-                        <TheListItem>Ditto</TheListItem>
-                        <TheListItem>Taillow</TheListItem>
-                        <TheListItem>Patrat</TheListItem>
-                        <TheListItem>Deerling</TheListItem>
-                        <TheListItem>Stufful</TheListItem>
-                        <TheListItem>Wooloo</TheListItem>
-                        <TheListItem>Lechonk</TheListItem>
+                        {pokeList.map((pokemon, index) => (
+                            <TheListItem key={index}>
+                                <ListItemText>{pokemon}</ListItemText>
+                            </TheListItem>
+                        ))}
                     </PokemonList>
                 </ListPaper>
             </ListDiv>
-
             <DetailsDiv>
                 <DataTable />
             </DetailsDiv>
         </MainDiv>
     );
 }
-
-/*
-
-<DetailsPaper>
-                    <DetailsList>
-                        <TheListItem><strong>Name: </strong>Rattata</TheListItem>
-                        <TheListItem><strong>DexNo: </strong>19</TheListItem>
-                        <TheListItem><strong>Type: </strong>normal</TheListItem>
-                        <TheListItem><strong>Egg-Group: </strong>field</TheListItem>
-                        <TheListItem><strong>Base-Stat: </strong>253</TheListItem>
-                        <TheListItem><strong>Abilities: </strong>run away, guts</TheListItem>
-                    </DetailsList>
-                </DetailsPaper>
-
-
-*/
